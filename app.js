59e3026d2a306a63b68877d970a3fca09942c955
anehellyperrony <153025974+Anehelly@users.noreply.github.com>
2024-06-10 19:32:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 window.alert('Bem vindo(a) ao jogo do número secreto');
 let numeroSecreto = 9;
-console.log(numeroSecreto)
-let numeroChute = window.prompt('Informe um número de 1 a 100:');
+let numeroChute; 
+let tentativa = 1;
+
+while (numeroChute != numeroSecreto){
+numeroChute = window.prompt('Informe um número de 1 a 10:');
 if(numeroChute==numeroSecreto){
-    alert (`Parabéns, você descobriu o número secreto: ${numeroSecreto}`);
+    alert (`Parabéns, você descobriu o número secreto: ${numeroSecreto} com ${tentativa} tentativas`);
 }else{
     alert('Você errou :(')
+    if(numeroChute > numeroSecreto){
+        alert(`O número secreto é menor que ${numeroChute}`);
+    }else{
+        alert(`O número secreto é maior que ${numeroChute}`);
+    }
+    tentativa++;
+    }
 }
 
